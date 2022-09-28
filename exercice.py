e6b57c3a8af64c8f93caf7646314533341762734
lemark <marcantoine_manningham.com>
2022-09-28 15:43:37 -0400

    Done

--- exercice.py
+++ exercice.py
@@ -3,16 +3,66 @@
 
 
 def check_brackets(text, brackets):
-	return False
+	to_close = [""]
+	opener = dict(zip(brackets[0::2], brackets[1::2]))
+	for char in text:
+		if char in opener:
+			to_close.append(char)
+		elif opener[to_close[-1]] == char:
+			to_close.pop()
+	return to_close == [""]
 
 def remove_comments(full_text, comment_start, comment_end):
-	return ""
+	comments_to_remove = []
+	in_comment = False
+	error = False
+	for i in range(len(full_text)):
+		if full_text[i:i+len(comment_start)] == comment_start and not in_comment:
+			to_remove_start = i
+			in_comment = True
+		elif full_text[i:i+len(comment_start)] == comment_start and in_comment:
+			error = True
+		elif full_text[i:i+len(comment_end)] == comment_end and in_comment:
+			to_remove_end = i
+			comments_to_remove.append((to_remove_start,to_remove_end))
+			in_comment = False
+		elif full_text[i:i+len(comment_end)] == comment_end and not in_comment:
+			error = True
+	
+	if error or in_comment:
+		full_text = None
+	elif comments_to_remove != []:
+		for start, end in comments_to_remove[::-1]:
+			full_text = full_text[:start] + full_text[end+2:]
+
+	return full_text
 
 def get_tag_prefix(text, opening_tags, closing_tags):
+	for opening_tag in opening_tags:
+		if text[0:len(opening_tag)] == opening_tag:
+			return (opening_tag, None)
+	for closing_tag in closing_tags:
+		if text[0:len(closing_tag)] == closing_tag:
+			return (None, closing_tag)
 	return (None, None)
 
 def check_tags(full_text, tag_names, comment_tags):
-	return False
+	to_close = [""]
+	full_text = remove_comments(full_text, "<!--", "-->")
+	if full_text == None:
+		return False
+	for i in range(len(full_text)):
+		if full_text[i] == "<":
+			if full_text[i+1] == "/":
+					if full_text[i+2:i+len(to_close[-1])+3] == to_close[-1] + ">":
+						to_close.pop()
+					else:
+						return False
+			for tag_name in tag_names:
+				if full_text[i+1:i+len(tag_name)+2] == tag_name + ">":
+					to_close.append(tag_name)
+	return to_close == [""]
+
 
 
 if __name__ == "__main__":
